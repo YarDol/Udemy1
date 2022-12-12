a277efcd4442a411edfa0db13b3e57646e9d8b02
Yar <slvwinswim@gmail.com>
2022-12-12 16:28:55 +0200

    add git ignore

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 "use strict";
 
-const category = 'toys';
+console.log('arr'+"- jbject");
+console.log(4 + +"5");
 
-console.log(`https://someurl.com/${category}/5`);
+let incr = 10,
+    dcr = 10;
 
-const user = 'Ivan';
+//++incr;
+//--dcr;
 
-alert(`Hi,${user}`);
+console.log(++incr);
+console.log(--dcr);
+
+console.log(5%2);
+
+console.log(2+2*2 == 8);
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && !isClose);
